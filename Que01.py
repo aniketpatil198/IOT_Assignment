c8c5f1b74f6eb16069adda46c581720213c33192
aniketpatil198 <aniket.patil.712819@gmail.com>
2024-04-17 12:32:40 +0530

    Assignment1 Que1 completed

---
+++ Que01.py
@@ -0,0 +1,8 @@
+#1] Write a Python Program find an area of a rectangle and perimeter of the rectangle.
+
+le = float(input("Enter length of rectangle : "))
+wd = float(input("Enter width of rectangle : "))
+
+area = le * wd
+
+print(f"Area of rectangle = {area}sq.cm")
